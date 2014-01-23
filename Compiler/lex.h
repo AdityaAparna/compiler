b65f06c9aedfc69443db130d51365a85f5f8f026
harshil93 <lodhi.harshil@gmail.com>
2014-01-23 14:59:47 -0800

    First Working Parser on VS

---
+++ Compiler/lex.h
@@ -0,0 +1,25 @@
+#define EOI				0	/* End of input			*/
+#define SEMI			1	/* ; 					*/
+#define PLUS 			2	/* + 					*/
+#define MINUS 			3	/* - 					*/
+#define MUL				4	/* * 					*/
+#define DIV				5	/* / 					*/
+#define LP				6	/* (					*/
+#define RP				7	/* )					*/
+#define ASSIGN			8	/* =					*/
+#define LESS			9	/* <					*/
+#define GREATER			10	/* >					*/
+#define EQ				11	/* ==					*/
+#define NUM				12	/* Decimal Number  		*/
+#define ID				13	/* identifier  			*/
+#define IF				14	/* if   				*/
+#define THEN			15	/* then   				*/
+#define WHILE			16	/* while   				*/
+#define DO				17	/* do  					*/
+#define BEGIN			18	/* begin   				*/
+#define END				19	/* end  				*/
+#define ERR				-1	/* Error  				*/
+
+extern char *yytext;		/* in lex.c	*/
+extern int yyleng;
+extern yylineno;
---
+++ Compiler/main.c
@@ -0,0 +1,6 @@
+main()
+{
+	stmt_list();
+	getch();
+
+}
